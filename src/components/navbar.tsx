3f2d724619f8f6cdb9ef592bca935f9e2636eb52
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-10-08 23:56:16 +0200

    style: Navbar overflow fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -20,12 +20,12 @@ const navigation = [
 export default function Navbar() {
   return (
     <nav className='flex items-center justify-between bg-background py-2'>
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center gap-2 w-full'>
         <h1 className='mr-4 text-2xl font-semibold uppercase text-red-600'>
           nextflix
         </h1>
 
-        <ul className='flex items-center gap-2'>
+        <ul className='flex items-center gap-2 overflow-x-auto w-full'>
           {navigation.map(({ link, name }, i) => (
             <li key={i}>
               <Button variant={'ghost'} asChild>
