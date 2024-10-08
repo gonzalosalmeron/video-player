7fdb24b28418f525d4cd4104b9847ce2cdf24606
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-10-08 23:58:32 +0200

    style: Change loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ import { Movie } from '@/types'
 export default function Home() {
   const { isLoading, data } = trpc.getMovies.useQuery()
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) {
+    return (
+      <div className='flex h-screen flex-col items-center justify-center'>
+        <h1 className='text-3xl font-bold'>Loading...</h1>
+      </div>
+    )
+  }
 
   return (
     <div className='py-10'>
