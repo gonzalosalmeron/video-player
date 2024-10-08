c10802523af3dded4b4e27c6d8967d3948b48e72
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-10-08 18:02:46 +0200

    feat: Add navbar component

---
+++ src/components/navbar.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+
+const navigation = [
+  {
+    link: '/',
+    name: 'Home',
+  },
+  {
+    link: '/series',
+    name: 'Series',
+  },
+  {
+    link: '/movies',
+    name: 'Movies',
+  },
+]
+
+export default function Navbar() {
+  return (
+    <nav className='flex items-center justify-between bg-background py-2'>
+      <div className='flex items-center gap-2'>
+        <h1 className='mr-4 text-2xl font-semibold uppercase text-red-600'>
+          nextflix
+        </h1>
+
+        <ul className='flex items-center gap-2'>
+          {navigation.map(({ link, name }, i) => (
+            <li key={i}>
+              <Button variant={'ghost'} asChild>
+                <Link href={link} className='flex items-center gap-1'>
+                  {name}
+                </Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
