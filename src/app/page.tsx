f370bf19952d53be5b311ab4bf5a7ee03e789533
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-10-08 19:52:58 +0200

    feat: Show movie list on principal path && new tRPC endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,63 @@
-import Image from "next/image";
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import trpc from 'trpc'
+
+import { Movie } from '@/types'
+
+const imgBasePath = process.env.NEXT_PUBLIC_TMDB_IMG_URL
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const { isLoading, data } = trpc.getMovies.useQuery()
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div className='py-10'>
+      <h4 className='pb-4 text-lg font-semibold'>Popular movies</h4>
+      <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-8'>
+        {data?.results?.map((movie: Movie, i: number) => (
+          <Link
+            href={`/movie/${movie?.id}`}
+            key={i}
+            className='group relative duration-200 hover:scale-105'
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={`${imgBasePath}/${movie?.poster_path}`}
+              alt={movie?.title}
+              width={200}
+              height={400}
+              className='h-auto w-full'
+              priority={false}
+              placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+            <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t from-background to-transparent px-4 pb-1 pt-6'>
+              <p>{movie?.title}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,20 @@
 import { publicProcedure, router } from './trpc'
 
+import { ResponseTMDB } from '@/types'
+
+const apiUrl = process.env.TMDB_URL ?? ''
+const apiKey = process.env.TMDB_API_KEY ?? ''
+
 export const appRouter = router({
   getMovies: publicProcedure.query(async () => {
-    return 10
+    try {
+      const res = await fetch(`${apiUrl}/movie/popular?api_key=${apiKey}`)
+      const data: ResponseTMDB = await res.json()
+      return data
+    } catch (error) {
+      console.error('Error fetching movies:', error)
+      throw new Error('Failed to fetch movies')
+    }
   }),
 })
 
