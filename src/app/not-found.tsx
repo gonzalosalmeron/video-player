400b6fb9ed6111cbc65883a0c34d758ddf9e2f55
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-10-08 20:58:07 +0200

    feat: Custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <h1 className='mb-8 text-3xl font-bold'>404 - Not Found</h1>
+      <p className='text-lg'>The page you are looking for does not exist.</p>
+    </div>
+  )
+}
